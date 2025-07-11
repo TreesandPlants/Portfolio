cf8e291035fb34c6d83f88d92aa2e3d3816a0746
TreesandPlants <parthiban07nature@gmail.com>
2025-07-11 12:13:59 +0530

    Add files via upload

---
+++ First step.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    
+    setTimeout(() => {
+        document.getElementById("loader").classList.add("hidden");
+    }, 500); 
+   
+    });
